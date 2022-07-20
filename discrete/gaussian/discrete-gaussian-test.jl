7487ca2dc66fc350ccd92c95f7b215243b442315
Edwin Ng <ngedwin98@gmail.com>
2022-07-19 18:45:20 -0700

    Fix target amplitude scaling parameter, switch to n_sat instead of zeta, and working parameters for n_sat ~ 0.1(zeta ~ 3)

--- discrete/gaussian/discrete-gaussian-test.jl
+++ discrete/gaussian/discrete-gaussian-test.jl
@@ -18,6 +18,7 @@ function simulate(csm, β, G, A, T_sim, N_traj, rng; kwargs...)
         q[l] = Vector{Vector{Float64}}(undef, T_sim)
         σ²[l] = deepcopy(q[l]); w[l] = deepcopy(q[l]); e[l] = deepcopy(q[l])
         for t ∈ 1 : T_sim
+            println("Traj $l, t = $t")
             roundtrip!(csm, β, G, A, rng; kwargs...)
             q[l][t] = [Γi.μ[1] for Γi in csm.Γ]
             σ²[l][t] = [Γi.Σ[1,1] for Γi in csm.Γ]
@@ -28,33 +29,35 @@ function simulate(csm, β, G, A, T_sim, N_traj, rng; kwargs...)
     return q, σ², w, e
 end
 
-# System parameters
-T_decay = 16  # Cavity decay time, in roundtrips
+# Set system parameters
+T_decay = 16 # Cavity decay time, in roundtrips
 η = 1         # Escape efficiency
 r = 1.5       # Pump parameter
-ς = 1      # Saturation parameter
-β_cac = 0.5   # CAC gain rate, in 1/T_decay
-α_cac = 1     # CAC target photon number, in units of n_sat
+n_sat = 1e-1  # Saturation photon number
+β_cac = 0.05   # CAC gain rate, in 1/T_decay
+α_cac = 1.0   # CAC target photon number, in units of n_sat
 
-# Simulation parameters
-T_sim = 50 * T_decay
+# Set simulation parameters
+T_sim = 20 * T_decay
 N_traj = 1
-seed = 0
+seed = rand(UInt32)
 
-# Process parameters
+# Calculate OPO parameters
 R_out = η * (1 - exp(-2/T_decay))
 R_loss = (1/η - 1) * R_out / (1 - R_out)
-ϵτ_nl = sqrt(8 / T_decay * 2ς^2)
-β = r * sqrt(2) / ϵτ_nl / T_decay
-κ = R_out / 2; g = ϵτ_nl^2 / 4; p = r / T_decay
-μ² = α_cac / 2ς^2
-cfs = [p-g/9*μ², 2(p-1-κ-g/3*μ²), -4κ]
-G = (β_cac/T_decay) / R_out / 2μ²
+ϵτ_nl = sqrt(4 / T_decay / n_sat)
+β = sqrt(2) * r / ϵτ_nl / T_decay
+
+# Generate feedback parameters
+n_cac = α_cac * n_sat
+G = (β_cac/T_decay) / R_out / 2n_cac
+κ = R_out/2; g = ϵτ_nl^2/4; p = r/T_decay
+cfs = [p - n_cac*g/9, 2(p-(1+κ)-n_cac*g/9), -4κ]
 δ = 0.5 + R_out * (roots∘Polynomial)(cfs)[2]
-A = 2μ² * R_out + δ
+A = 2n_cac * R_out + δ
 
 # Load problem
-C = load_cnf(joinpath("..","SAT_problems","random3SATn50a3.42.cnf"))
+C = load_cnf(joinpath(pwd(),"SAT_problems","random3SATn50a3.42.cnf"))
 M, N = size(C)
 problem = process_clauses(C)
 
@@ -64,20 +67,24 @@ opo = OPO(GaussianGainModel(ϵτ_nl), R_loss, R_out)
 csm = CSM(opo, problem)
 q, σ², w, e = simulate(csm, β, G, A, T_sim, N_traj, rng);
 
+# Plot results
 fig, axs = plt.subplots(2, 2, figsize=(10,6))
-axs[1,1].plot(q[1])
-axs[1,2].plot(σ²[1])
-axs[2,1].plot(e[1])
-axs[2,2].plot(w[1])
+axs[1,1].plot(q[1], alpha=0.3)
+axs[1,2].plot(σ²[1], alpha=0.3)
+axs[2,1].plot(e[1], alpha=0.3)
+axs[2,2].plot(w[1], alpha=0.3)
 
 axs[1,1].set_ylim(maximum(abs.(axs[1,1].get_ylim())) * [-1,1])
-axs[1,2].set_ylim(0, 2)
+axs[1,2].set_ylim(0, axs[1,2].get_ylim()[2])
 axs[2,1].set_ylim(0, axs[2,1].get_ylim()[2])
 axs[2,2].set_ylim(maximum(abs.(axs[2,2].get_ylim())) * [-1,1])
+
+axs[1,1].axhline(sqrt(α_cac * 2n_sat), linestyle="--", color=:k)
+axs[1,1].axhline(-sqrt(α_cac * 2n_sat), linestyle="--", color=:k)
 
 for i ∈ 1 : size(axs,1)
     for j ∈ 1 : size(axs,2)
         axs[i,j].set_xlim(0, T_sim)
-        axs[i,j].tick_params(labelleft=j==1, labelbottom=)
+        axs[i,j].tick_params(labelbottom=i==size(axs,1))
     end
 end
